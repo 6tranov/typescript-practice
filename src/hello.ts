11bc26dc2f771195d39104f156b7e6e5242c45d3
6tranov <6tranov@yahoo.co.jp>
2021-12-11 03:25:03 +0900

    :+1:feat use class
    try oop.
    
      - :+1:feat
        A new feature
      - :bug:fix
        A bug fix
      - :memo:docs
        Documentation only changes
      - :lipstick:style
        Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
      - :recycle:refactor
        A code change that neither fixes a bug nor adds a feature
      - :racehorse:perf
        A code change that improves performance
      - :white_check_mark:test
        Adding missing or correcting existing tests
      - :construction_worker:chore
        Changes to the build process or auxiliary tools and libraries such as documentation generation

--- src/hello.ts
+++ src/hello.ts
@@ -1,2 +1,14 @@
-const message:string = 'hello world';
+const message: string = 'hello world';
 console.log(message);
+
+class Person {
+    name: string = '';
+    id: number = 0;
+
+    greeting(): string {
+        return `Hello, my name is ${this.name}`;
+    }
+}
+
+const person = new Person();
+console.log(person.greeting());
